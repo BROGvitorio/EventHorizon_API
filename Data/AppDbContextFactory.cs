8e5ef57b20bd40a9036865c71cedb1b0bb0bf61b
Melissa Vitorio <melissavitorio.me@gmail.com>
2026-05-04 09:12:02 -0300

    saving

--- Data/AppDbContextFactory.cs
+++ Data/AppDbContextFactory.cs
@@ -1,6 +1,25 @@
-﻿namespace EventHorizon_API.Data
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace EventHorizon_API.Data
 {
-    public class AppDbContextFactory
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
     {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Path.Combine(Directory.GetCurrentDirectory()))
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("AppDbConnectionString");
+
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            var serverVersion = new MySqlServerVersion(new Version(8, 0, 44));
+            optionsBuilder.UseMySql(connectionString, serverVersion);
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
     }
 }
