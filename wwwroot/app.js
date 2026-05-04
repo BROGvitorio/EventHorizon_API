2ca6ff21041ddccd1b91609900ddbb90f0b9fec5
Melissa Vitorio <melissavitorio.me@gmail.com>
2026-05-04 09:55:05 -0300

    saving

---
+++ wwwroot/app.js
@@ -0,0 +1,22 @@
+﻿const apiUrl = '/api/EventHorizon';
+async function carregarContas() {
+    try {
+        const resposta = await fetch(apiUrl);
+        const bankAccounts = await resposta.json();
+
+        const tbody = document.getElementById('bankAccountsTable');
+        tbody.innerHTML = "";
+
+        bankAccounts.forEach(bankAccount => {
+            tbody.innerHTML += `
+                <tr>
+                    <td>${bankAccount.userId}</td>
+                    <td>${bankAccount.category}</td>
+                    <td>${bankAccount.balance}</td>
+                </tr>
+            `
+        })
+    } catch {
+        console.log("Erro ao exibir contas.");
+    }
+}
